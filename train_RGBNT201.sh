995e2fe43b36d0e45633b74334a4030ed69bec3d
BraveApple <applewangpingyu@gmail.com>
2024-04-23 11:10:10 +0800

    init

--- train_RGBNT201.sh
+++ train_RGBNT201.sh
@@ -2,6 +2,7 @@
 
 set -e
 
-CUDA_VISIBLE_DEVICES=3 python train_net.py \
+CUDA_VISIBLE_DEVICES='1' \
+    python train_net.py \
     --config_file configs/RGBNT201/EDITOR.yml \
     SOLVER.IMS_PER_BATCH 64
